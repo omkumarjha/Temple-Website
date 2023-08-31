87eff68140a4c456f8f4c8abac4fff7dbbcf591a
om <omkumar1892002@gmail.com>
2023-08-31 08:09:02 +0530

    Added donation section for clients

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const formSubmit = () => {
+    let nameInput = document.querySelector(".nameInput");
+    let mailInput = document.querySelector(".mailInput");
+    let messsageInput = document.querySelector(".messageInput");
+
+    nameInput.value = ""; 
+    mailInput.value = ""; 
+    messsageInput.value = ""; 
+}
