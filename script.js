49070c63f0189589446eaa796c6110aef2841039
om <omkumar1892002@gmail.com>
2023-09-01 11:17:25 +0530

    Made navbar responsive

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ const formSubmit = () => {
     mailInput.value = ""; 
     messsageInput.value = ""; 
 }
+
+// For hamburger
+
+let hamburger = document.querySelector(".hamburger");
+let logo = document.querySelector(".logo");
+let ul = document.querySelector(".ul");
+let navbar = document.getElementById("navbar");
+
+hamburger.addEventListener("click",()=>{
+    logo.classList.toggle("v-hidden")
+    ul.classList.toggle("v-hidden")
+    navbar.classList.toggle("h-navbar")
+})
