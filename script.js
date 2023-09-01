5a77042e8b118eeef0cbe7a83f67224075cd8769
om <omkumar1892002@gmail.com>
2023-09-01 18:48:06 +0530

    Improved the design and js logic

--- script.js
+++ script.js
@@ -22,22 +22,15 @@ hamburger.addEventListener("click",()=>{
     navbar.classList.toggle("h-navbar")
 })
 
-// below code mai bas jaise hi screen ki width 600 px ho jayegi to hum  "welcome to" ke niche bache hue text ko laa denge .
+// below code mai bas jaise hi screen ki width 600 px ya usse kam ho jayegi to hum  "welcome to" ke niche bache hue text ko laa denge .
 
-// This media query targets viewports that have a maximum width of 600px
-const mQuery = window.matchMedia('(max-width: 600px)')
-
-function handleMobilePhoneResize(e) {   
-   // Check if the media query is true
-   if (e.matches) {     
+setInterval(()=>{
+    if(window.screen.availWidth < 600){
         let right = document.querySelector(".right");
         right.children[0].innerHTML = "Welcome To <br> Baijantri Shakti Peeth Trust";
-   }
-   else{
+    }
+    else{
         let right = document.querySelector(".right");
         right.children[0].innerHTML = "Welcome To Baijantri Shakti Peeth Trust";
-   }
-} 
-
-// Set up event listener 
-mQuery.addListener(handleMobilePhoneResize)
+    }
+},100)
